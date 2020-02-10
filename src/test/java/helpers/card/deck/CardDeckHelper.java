558c75ebf570a212c7c7e4d2d1e39f81c2c8c116
dmitryl <dmitrijs.litvinenko@neotech.lv>
2020-02-10 19:02:19 +0200

    adding: draw a card method + update: shuffle card

--- src/test/java/helpers/card/deck/CardDeckHelper.java
+++ src/test/java/helpers/card/deck/CardDeckHelper.java
@@ -3,14 +3,30 @@ package helpers.card.deck;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 
-import static configuration.PropertiesConfiguration.getCardDeckUrl;
 import static configuration.PropertiesConfiguration.getShuffleUrlPath;
+import static helpers.UrlHelper.buildDeckUrl;
 
 public class CardDeckHelper {
 
-    public Response shuffleCards(int deckCount) {
+    private static final String DECK_COUNT_PARAM = "deck_count";
+    private static final String DRAW_CARD_COUNT = "count";
+
+    public Response shuffleCards(final int deckCount) {
+        var url = buildDeckUrl().path(getShuffleUrlPath())
+                .queryParam(DECK_COUNT_PARAM, deckCount).build().toUriString();
+
         return RestAssured.given()
-                .get(getCardDeckUrl() + getShuffleUrlPath() + "?deck_count=" + deckCount)
+                .get(url)
+                .then().statusCode(200)
+                .and().extract().response();
+    }
+
+    public Response drawCard(final String deckId, final int deckNumber) {
+        var url = buildDeckUrl().path("api/deck/" + deckId + "/draw/")
+                .queryParam(DRAW_CARD_COUNT, deckNumber).build().toUriString();
+
+        return RestAssured.given()
+                .get(url)
                 .then().statusCode(200)
                 .and().extract().response();
     }
